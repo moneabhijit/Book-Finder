9e1f5b8c02f316338b3dbb05cced54acbe47ef75
anaghasalvi <salvianagha1512@gmail.com>
2020-06-28 17:54:37 +0530

    adding fields to form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
 }
